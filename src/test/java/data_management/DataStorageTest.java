8720715c201f25430f686d5f1dc8a603bebef504
Joachim-06 <joachimnanoukon@gmail.com>
2025-04-08 17:56:14 +0200

    Update DataStorageTest.java

--- src/test/java/data_management/DataStorageTest.java
+++ src/test/java/data_management/DataStorageTest.java
@@ -1,7 +1,7 @@
 package data_management;
 
-import static org.junit.jupiter.api.Assertions.*;
-import org.junit.jupiter.api.Test;
+//import static org.junit.jupiter.api.Assertions.*;
+//import org.junit.jupiter.api.Test;
 
 import com.data_management.DataStorage;
 import com.data_management.PatientRecord;
