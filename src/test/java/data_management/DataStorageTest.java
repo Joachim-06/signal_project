657af75ed5de184e2cfb09ff2c10317c2fb83fa5
Joachim-06 <joachimnanoukon@gmail.com>
2025-04-08 18:15:38 +0200

    Update DataStorageTest.java

--- src/test/java/data_management/DataStorageTest.java
+++ src/test/java/data_management/DataStorageTest.java
@@ -1,7 +1,7 @@
 package data_management;
 
-//import static org.junit.jupiter.api.Assertions.*;
-//import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.Test;
 
 import com.data_management.DataStorage;
 import com.data_management.PatientRecord;
@@ -14,6 +14,15 @@ class DataStorageTest {
     void testAddAndGetRecords() {
         // TODO Perhaps you can implement a mock data reader to mock the test data?
         // DataReader reader
+        DataReader reader = new DataReader() {
+            @Override
+            public List<PatientRecord> readData() {
+                // return some mock test data
+                List<PatientRecord> mockRecords = new ArrayList<>();
+                mockRecords.add(new PatientRecord(1, 100.0, "WhiteBloodCells", 1714376789050L));
+                return mockRecords;
+            }
+        };
         DataStorage storage = new DataStorage(reader);
         storage.addPatientData(1, 100.0, "WhiteBloodCells", 1714376789050L);
         storage.addPatientData(1, 200.0, "WhiteBloodCells", 1714376789051L);
@@ -22,4 +31,6 @@ class DataStorageTest {
         assertEquals(2, records.size()); // Check if two records are retrieved
         assertEquals(100.0, records.get(0).getMeasurementValue()); // Validate first record
     }
+
+
 }
