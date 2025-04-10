2ee1813540c1b76df90f911f996da94d43547265
Joachim-06 <joachimnanoukon@gmail.com>
2025-04-10 12:37:51 +0200

    Update DataStorageTest.java

--- src/test/java/data_management/DataStorageTest.java
+++ src/test/java/data_management/DataStorageTest.java
@@ -41,7 +41,9 @@ class DataStorageTest {
                 }
             }
         };
-        DataStorage storage = new DataStorage(reader);
+        DataStorage storage = new DataStorage();
+        reader.readData(storage);
+        
         storage.addPatientData(1, 100.0, "WhiteBloodCells", 1714376789050L);
         storage.addPatientData(1, 200.0, "WhiteBloodCells", 1714376789051L);
 
