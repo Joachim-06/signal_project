6b1ac3c85d3209128c18936df7985a03e791a409
Joachim-06 <joachimnanoukon@gmail.com>
2025-04-10 15:43:56 +0200

    Add documentation to PatientDataGenerator.java

--- src/main/java/com/cardio_generator/generators/PatientDataGenerator.java
+++ src/main/java/com/cardio_generator/generators/PatientDataGenerator.java
@@ -2,6 +2,16 @@ package com.cardio_generator.generators;
 
 import com.cardio_generator.outputs.OutputStrategy;
 
+/**
+* Generates patient data
+*
+*/
 public interface PatientDataGenerator {
+    /**
+    * Generates patient data
+    *
+    *@param patientId The id of the patient
+    *@param outputStrategy The strategy used to output the generated data
+    */
     void generate(int patientId, OutputStrategy outputStrategy);
 }
