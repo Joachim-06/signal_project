e16f4acfa574b3fab13a1162325e400c145fc9de
Joachim-06 <joachimnanoukon@gmail.com>
2025-04-10 15:50:19 +0200

    Add documentation to OutputStrategy.java

--- src/main/java/com/cardio_generator/outputs/OutputStrategy.java
+++ src/main/java/com/cardio_generator/outputs/OutputStrategy.java
@@ -1,5 +1,18 @@
 package com.cardio_generator.outputs;
 
+/**
+* Gives the type of strategy used to generate the output (eg: console or file)
+*
+*/
 public interface OutputStrategy {
+
+    /**
+    * Generates the output with the chosen output strategy
+    *
+    *@param patientId The Id of the patient
+    *@param timestamp The timestamp
+    *@param label The label chosen
+    *@param data The data that is going to be generated in the output
+    */
     void output(int patientId, long timestamp, String label, String data);
 }
