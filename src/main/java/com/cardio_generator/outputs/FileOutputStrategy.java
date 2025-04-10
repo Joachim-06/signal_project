bd1c46cc51b55f0eac15bfed4dde63c0ac6b3061
Joachim-06 <joachimnanoukon@gmail.com>
2025-04-10 14:53:30 +0200

    Update and rename fileOutputStrategy.java to FileOutputStrategy.java

---
+++ src/main/java/com/cardio_generator/outputs/FileOutputStrategy.java
@@ -0,0 +1,46 @@
+package com.cardio_generator.outputs;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.nio.file.StandardOpenOption;
+import java.util.concurrent.ConcurrentHashMap;
+
+// Changed class name to UpperCamelCase
+public class FileOutputStrategy implements OutputStrategy {
+
+    // Changed variable name to lowerCamelCase
+    private String baseDirectory;
+
+    // Changed variable name to UPPER_SNAKE_CASE
+    public final ConcurrentHashMap<String, String> FILE_MAP = new ConcurrentHashMap<>();
+
+    // Changed class constructor name to UpperCamelCase
+    public FileOutputStrategy(String baseDirectory) {
+
+        this.baseDirectory = baseDirectory;
+    }
+
+    @Override
+    public void output(int patientId, long timestamp, String label, String data) {
+        try {
+            // Create the directory
+            Files.createDirectories(Paths.get(baseDirectory));
+        } catch (IOException e) {
+            System.err.println("Error creating base directory: " + e.getMessage());
+            return;
+        }
+        // Set the FilePath variable
+        // Changed variable to lowerCamelCase
+        String filePath = FILE_MAP.computeIfAbsent(label, k -> Paths.get(baseDirectory, label + ".txt").toString());
+
+        // Write the data to the file
+        try (PrintWriter out = new PrintWriter(
+                Files.newBufferedWriter(Paths.get(filePath), StandardOpenOption.CREATE, StandardOpenOption.APPEND))) {
+            out.printf("Patient ID: %d, Timestamp: %d, Label: %s, Data: %s%n", patientId, timestamp, label, data);
+        } catch (Exception e) {
+            System.err.println("Error writing to file " + filePath + ": " + e.getMessage());
+        }
+    }
+}
