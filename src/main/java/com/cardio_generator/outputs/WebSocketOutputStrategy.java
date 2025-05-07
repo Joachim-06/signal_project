2808f910e03d2cb231c5547cb4fa8b6185785a9d
Joachim-06 <joachimnanoukon@gmail.com>
2025-05-07 18:15:26 +0200

    quick changes

--- src/main/java/com/cardio_generator/outputs/WebSocketOutputStrategy.java
+++ src/main/java/com/cardio_generator/outputs/WebSocketOutputStrategy.java
@@ -27,8 +27,8 @@ public class WebSocketOutputStrategy implements OutputStrategy {
     @Override
     public void output(int patientId, long timestamp, String label, String data) {
         if (label.equals("Saturation")) {
-            double temp = Double.parseDouble(data.replace("%", ""))/100;
-            data = String.valueOf(temp);
+            //double temp = Double.parseDouble(data.replace("%", ""))/100;
+            //data = String.valueOf(temp);
         } else if (label.equals("Alert")) {
             if (data.equals("triggered")) {
                 data = "0";
